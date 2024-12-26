2ee6834aecc1d4528fc82c490e26d6d9fcb58ea3
Sudhirkumar19 <chinna257@gmail.com>
2024-12-26 23:10:37 +0530

    concept

--- install_docker.sh
+++ install_docker.sh
@@ -4,4 +4,5 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl start docker
+systemctl enable docker
 usermod -aG docker ec2-user
